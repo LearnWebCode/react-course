3cf2e9b18e39987c71fd468ad57de5584c9b384a
Brad Schiff <LearnWebCode@users.noreply.github.com>
2022-08-31 10:37:26 -0700

    Update db.js

--- backend-api/db.js
+++ backend-api/db.js
@@ -1,12 +1,14 @@
 const dotenv = require("dotenv")
 dotenv.config()
-const mongodb = require("mongodb")
+const { MongoClient } = require("mongodb")
 
-mongodb.connect(process.env.CONNECTIONSTRING, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, client) {
-  if (err) {
-    console.log(err)
-  }
+const client = new MongoClient(process.env.CONNECTIONSTRING)
+
+async function start() {
+  await client.connect()
   module.exports = client
   const app = require("./app")
   app.listen(process.env.PORT)
-})
+}
+
+start()
